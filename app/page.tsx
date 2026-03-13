7db3e363175db77cebeb83cf8487b9c5d7d212cc
Dan <dan.montano@oliveandvinehk.com>
2026-03-13 11:22:20 +0800

    chore: default page redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div className="min-h-screen bg-white">
-      <main className="flex min-h-screen flex-col items-center justify-center p-8">
-        <h1 className="text-2xl font-semibold text-black">Home</h1>
-      </main>
-    </div>
-  );
+  redirect("/module-selection");
 }
